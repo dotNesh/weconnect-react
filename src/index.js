ba836a860145c687725cbb3533881e81966612ea
Kelvin Kariuki <kelvin.kariuki@andela.com>
2018-06-29 07:59:29 +0300

    [ft] import css for toast-notify

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import 'react-toastify/dist/ReactToastify.css';
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom';
+import store from './store';
 import './index.css';
 import App from './components/App';
 
-ReactDOM.render(<BrowserRouter>
+ReactDOM.render(<Provider store={store}>
+  <BrowserRouter>
     <App />
-</BrowserRouter>, document.getElementById('root'));
+  </BrowserRouter>
+</Provider>, document.getElementById('root'));
 
