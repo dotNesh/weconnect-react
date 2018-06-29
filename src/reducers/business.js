099e5b9038ae9b7ac92e3fc9d62c4d7e1c7baa66
Kelvin Kariuki <kelvin.kariuki@andela.com>
2018-06-29 08:44:08 +0300

    [ft] business reducer

---
+++ src/reducers/business.js
@@ -0,0 +1,63 @@
+import { ADD_BUSINESS, GET_BUSINESSES, GET_BUSINESS, POST_REVIEWS, EDIT_BUSINESS, DELETE_BUSINESS, GET_REVIEWS } from '../actions/types';
+
+const initialState = {
+  business: [],
+  businesses: [],
+  page: [],
+  message: [],
+  reviews: []
+};
+
+const BusinessReducer = (state = initialState, action) => {
+  switch (action.type) {
+  case ADD_BUSINESS:
+    return [
+      state, {
+        businessName: action.businessName,
+        category: action.category,
+        location: action.location,
+        description: action.description
+      }
+    ];
+
+  case GET_BUSINESSES:
+    return {
+      state,
+      businesses: action.payload.Businesses,
+      page: action.page
+    };
+
+  case GET_BUSINESS:
+    return {
+      state,
+      business: action.payloads,
+    };
+
+  case POST_REVIEWS:
+    return {
+      state,
+      message: action.response
+    };
+
+  case EDIT_BUSINESS:
+    return {
+      state,
+      message: action.message
+    };
+  case DELETE_BUSINESS:
+    return {
+      state,
+      message: action.message
+    };
+  case GET_REVIEWS:
+    return {
+      state,
+      reviews: action.payload
+    };
+
+  default:
+    return state;
+  }
+};
+
+export default BusinessReducer;
