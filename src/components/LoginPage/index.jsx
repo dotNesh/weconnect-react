73b5422315c73db4d9924b3ddde6f93dd1fa638d
Kelvin Kariuki <kelvin.kariuki@andela.com>
2018-06-29 07:52:29 +0300

    [ft] modified login page

--- src/components/LoginPage/index.jsx
+++ src/components/LoginPage/index.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import LoginForm from '../LoginForm';
 
 const LoginPage = () => (
-    <div className="container">
-        <div className="wrap forms">
-            <h2>Log into WeConnect</h2>
-            <LoginForm />
-        </div>
+  <div className="container">
+    <div className="wrap forms">
+      <h2>Log into WeConnect</h2>
+      <LoginForm />
     </div>
+  </div>
 
 );
 
