d5c5fd2610e93586494213d8ffc69a4231d0fdd5
Kelvin Kariuki <kelvin.kariuki@andela.com>
2018-06-30 08:54:47 +0300

    [ft] Changed homepage layout

--- src/components/HomeLayout/index.jsx
+++ src/components/HomeLayout/index.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import Hero from '../Hero';
 
 const HomeLayout = () => (
-    <div className="container">
-        <Hero />
-    </div>
+  <div className="container">
+    <Hero />
+  </div>
 );
 
 export default HomeLayout;
