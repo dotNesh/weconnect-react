e9d06a9db93f92c6e7539b442452a5e388f3a088
Kelvin Kariuki <kelvin.kariuki@andela.com>
2018-07-02 00:05:42 +0300

    [ft] about page

--- src/components/AboutPage/index.jsx
+++ src/components/AboutPage/index.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 
 const AboutPage = () => (
-    <div className="container">
-        <div className="jumbotron">
-            <h1 className="display-3">WeConnect</h1>
-            <hr className="my-4"/>
-            <p className="lead">"WeConnect provides a platform that brings businesses and individuals together.
+  <div className="container">
+    <div className="jumbotron">
+      <h1 className="display-3">WeConnect</h1>
+      <hr className="my-4"/>
+      <p className="lead">"WeConnect provides a platform that brings businesses and individuals together.
          This platform creates awareness for businesses and gives the users the ability to write reviews about the businesses they have interacted with."
-            </p>
-        </div>
+      </p>
     </div>
+  </div>
 
 );
 
